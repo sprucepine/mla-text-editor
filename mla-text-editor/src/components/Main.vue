8b69ca66e0e2d9a0686be6d8b9e5f1e4317343e6
Davis Volpe <warning-tintype.1f@icloud.com>
2026-07-04 15:02:23 +0000

    Add floating titles and add more padding

--- mla-text-editor/src/components/Main.vue
+++ mla-text-editor/src/components/Main.vue
@@ -24,17 +24,33 @@ onMounted(() => {
                     <div>
                         <h1 class="text-3xl font-semibold tracking-tight">Start writing</h1>
                     </div>
-                    
-                    <Card v-if="documentStore.activeDocument" title="Heading Information">
-                        <p class="pb-4">The heading in MLA format is displayed on the first page of your paper.</p>   
-                        
-                        <input v-model="documentStore.activeDocument.title" type="text" placeholder="Document Title" class="input input-bordered w-full mb-2" />
-                        <input v-model="documentStore.activeDocument.name" type="text" placeholder="Your Name" class="input input-bordered w-full mb-2" />
-                        <input v-model="documentStore.activeDocument.professor" type="text" placeholder="Professor's Name" class="input input-bordered w-full mb-2" />
-                        <input v-model="documentStore.activeDocument.course" type="text" placeholder="Course Name" class="input input-bordered w-full mb-2" />
-                        <input v-model="documentStore.activeDocument.dueDate" type="date" placeholder="Due Date" class="input input-bordered w-full mb-2" />
-                    </Card>
+
                     <template v-if="documentStore.activeDocument">
+
+
+                        <Card v-if="documentStore.activeDocument" title="Heading Information">
+                            <p class="pb-4">The heading in MLA format is displayed on the first page of your paper.</p>   
+                            <label class="floating-label">
+                                <span class="label-text">Document Title</span>
+                                <input v-model="documentStore.activeDocument.title" type="text" placeholder="Document Title" class="input input-bordered w-full mb-4" />
+                            </label>
+                            <label class="floating-label">
+                                <span class="label-text">Your Name</span>
+                                <input v-model="documentStore.activeDocument.name" type="text" placeholder="Your Name" class="input input-bordered w-full mb-4" />
+                            </label>
+                            <label class="floating-label">
+                                <span class="label-text">Professor's Name</span>
+                                <input v-model="documentStore.activeDocument.professor" type="text" placeholder="Professor's Name" class="input input-bordered w-full mb-4" />
+                            </label>
+                            <label class="floating-label">
+                                <span class="label-text">Course Name</span>
+                                <input v-model="documentStore.activeDocument.course" type="text" placeholder="Course Name" class="input input-bordered w-full mb-4" />
+                            </label>
+                            <label class="floating-label">
+                                <span class="label-text">Due Date</span>
+                                <input v-model="documentStore.activeDocument.dueDate" type="date" placeholder="Due Date" class="input input-bordered w-full mb-4" />
+                            </label>
+                        </Card>
                         <Card v-for="(block, index) in documentStore.activeDocument.content" :key="index" :title="'Paragraph'" :icon="'boxicons:paragraph'">
                             <textarea v-model="block.text" class="textarea textarea-bordered w-full" placeholder="  Type your content here..."></textarea>
                         </Card>
