587c17d3ca0d051b07a30ff17246bcb2b0357813
Davis Volpe <warning-tintype.1f@icloud.com>
2026-08-19 02:07:56 +0000

    chore: standardize import statements and format main.css; integrate Nuxt UI plugin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import App from './App.vue'
 import './main.css'
 import router from './router'
 import 'iconify-icon'
+import ui from '@nuxt/ui/vue-plugin'
 
 const app = createApp(App)
 const pinia = createPinia()
 
+app.use(ui)
 app.use(pinia)
 app.use(router)
 
