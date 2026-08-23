ef698713416d5c7736a136b604d585e8f190972c
Davis Volpe <warning-tintype.1f@icloud.com>
2026-08-23 17:04:47 +0000

    chore: enhance Navbar component with tooltip for document details and refactor button to UButton

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -63,13 +63,15 @@ function exportDocumentAsPdf() {
   <UHeader title="MLA Text Editor">
     <template #title>
       <span>MLA Text Editor</span>
+      <UTooltip :text="'Edit Document Details'" :content="{ side: 'bottom' }">
       <UDropdownMenu :items="documentNameOptions" class="ml-4">
-        <button class="btn btn-sm btn-ghost" title="Document Options">
+        <UButton variant="ghost" size="sm" color="neutral">
           <iconify-icon :icon="documentStore.activeDocument?.fileIcon || 'mdi-file-document-outline'" />
           <span class="ml-2">{{ documentStore.activeDocument?.fileTitle || 'Untitled Document' }}</span>
           <iconify-icon icon="mdi-chevron-down" class="ml-1" />
-        </button>
+        </UButton>
       </UDropdownMenu>
+      </UTooltip>
       <div v-if="documentStore.activeDocument" class="flex items-center">
          <div v-if="documentStore.saveState === 'saved'">
           <UTooltip :text="'All changes saved'" :content="{ side: 'bottom' }">
